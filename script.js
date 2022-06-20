25c0863d9876c1df645e9c8079a45cbc2f5ae9e5
nakkinn <81859514+nakkinn@users.noreply.github.com>
2022-06-20 22:51:03 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function request_permission(){
+    if (
+        DeviceMotionEvent &&
+        DeviceMotionEvent.requestPermission &&
+        typeof DeviceMotionEvent.requestPermission === 'function'
+    ) {
+        DeviceMotionEvent.requestPermission();
+    }
+    if (
+        DeviceOrientationEvent &&
+        DeviceOrientationEvent.requestPermission &&
+        typeof DeviceOrientationEvent.requestPermission === 'function'
+    ) {
+        DeviceOrientationEvent.requestPermission();
+    }
+
+}
+
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+    request_permission();
+}
+
+function draw(){
+    background(0);
+
+    fill(255);
+
+}
